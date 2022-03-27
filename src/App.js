b9f9a4fc33c8999c757804c4d6c88eb8bee56edc
yunosuke924 <y.minegishi0924@gmail.com>
2022-03-27 23:44:12 +0900

    add 動的にクエリ生成

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { ApolloProvider } from 'react-apollo'
 import client from './client'
 import { Query } from 'react-apollo';
 import { SEARCH_REPOSITORIES } from './graphql';
+import { useState } from 'react';
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
@@ -13,13 +14,18 @@ const VARIABLES = {
 }
 
 const App = () => {
-  const {first, after, last, before, query} = VARIABLES
+  // const {first, after, last, before, query} = DEFAULT_STATE
+  const [defaultState, setDefaultState] = useState(DEFAULT_STATE)
+  const {query} = defaultState
   return (
     <ApolloProvider client={client}>
+      <form>
+        <input value={query} onChange={(event)=> setDefaultState({...DEFAULT_STATE, query: event.target.value})}/>
+      </form>
       <div>
         hi GraphQL 20220327
       </div>
-      <Query query={SEARCH_REPOSITORIES} variables={{first, after, last, before, query}}>{
+      <Query query={SEARCH_REPOSITORIES} variables={defaultState}>{
         ({loading,error,data})=>{
           if (loading) return 'Loading...'
           if (error) return `Error! ${error.message}`
