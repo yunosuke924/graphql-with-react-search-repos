f731cd2811c2357bf25c58fc10710ec0f3cbfc84
yunosuke924 <y.minegishi0924@gmail.com>
2022-03-27 22:59:57 +0900

    add SEARCH_REPOSITORIESの追加

--- src/App.js
+++ src/App.js
@@ -2,24 +2,31 @@ import { ApolloProvider } from 'react-apollo'
 
 import client from './client'
 import { Query } from 'react-apollo';
-import {ME} from './graphql'
+import { SEARCH_REPOSITORIES } from './graphql';
 
+const VARIABLES = {
+  first: 5,
+  after: null,
+  last: null,
+  before: null,
+  query: "フロントエンドエンジニア"
+}
 
-function App() {
+const App = () => {
+  const {first, after, last, before, query} = VARIABLES
   return (
     <ApolloProvider client={client}>
       <div>
-        hi GraphQL
+        hi GraphQL 20220327
       </div>
-      <Query query={ME}>{
+      <Query query={SEARCH_REPOSITORIES} variables={{first, after, last, before, query}}>{
         ({loading,error,data})=>{
           if (loading) return 'Loading...'
           if (error) return `Error! ${error.message}`
-          return <div>{data.user.name}</div>
+          return <div></div>
         }
       }</Query>
-      </ApolloProvider>
-    
+    </ApolloProvider>
   )
 }
 
